17e273808087ef56e7aae0447ca7f80133f3ceea
psucodervn <hungle.info@gmail.com>
2023-09-21 22:05:13 +0700

    Update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,15 +26,24 @@ const challenges = [
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 bg-slate-800">
-      <div className="shadow-xl drop-shadow-xl rounded-xl bg-slate-900 text-white p-8">
+    <main className="flex min-h-screen flex-col items-center p-4 sm:p-24 bg-slate-800">
+      <div className="text-xl text-white p-4 shadow-xl drop-shadow-xl">
+        <span className="text-green-600 font-bold text-xl mx-3">√</span>
+        Completed Challenges
+      </div>
+      <div className="shadow-xl border-2 border-green-600 min-w-full sm:min-w-max sm:w-[30rem] drop-shadow-xl rounded-xl bg-slate-900 text-white p-4">
         <ul className="flex flex-col">
           {challenges.map((chall, idx) => (
-            <li key={chall.slug} className="p-3">
-              <Link href={chall.slug} key={chall.slug} className="flex gap-2">
-                <p>{idx + 1}.</p>
-                <p className="hover:text-green-600 transition-all">
+            <li key={chall.slug} className="p-1 sm:p-2">
+              <Link
+                href={chall.slug}
+                key={chall.slug}
+                className="flex gap-3 hover:text-green-600 transition-all group"
+              >
+                <p className="w-6 text-right">{idx + 1}.</p>
+                <p>
                   {chall.title}
+                  <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-green-600"></span>
                 </p>
               </Link>
             </li>
