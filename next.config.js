a09851953ddc1e5a29e3ad35273b0b9715976fea
psucodervn <hungle.info@gmail.com>
2023-09-26 15:16:43 +0700

    ci: update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: isGithubActions ? 'export' : 'standalone',
+  assetPrefix,
+  basePath,
 }
 
 module.exports = nextConfig
